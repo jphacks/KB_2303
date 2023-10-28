e5e46643bb0e1eefe2e5f3ee18796bfade87f153
inatatsu <tatsuhiro.9699@gmail.com>
2023-10-28 17:53:59 +0900

    [add] icon image component template

---
+++ client/src/components/Icon/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Root } from './Styles'
+
+type Props = {
+
+}
+
+export const Icon: React.FC<Props> = ({}) => {
+  return (
+    <Root>Icon component</Root>
+  )
+}

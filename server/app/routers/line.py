c53bc2ea12baab15d3ed3d32d40d968a94a64ab3
krn-7717 <104199555+krn-7717@users.noreply.github.com>
2023-10-28 21:50:53 +0900

    [wip]途中

--- server/app/routers/line.py
+++ server/app/routers/line.py
@@ -2,6 +2,9 @@ import os
 import sys
 
 from fastapi import Request, APIRouter, HTTPException
+from enum import Enum
+from crud.line_communication_state import LineCommunicationStateCrud
+from crud.schemas import LINECommunicationStateSchema
 
 from linebot.v3.webhook import WebhookParser
 from linebot.v3.messaging import (
@@ -16,12 +19,37 @@ from linebot.v3.exceptions import (
 )
 from linebot.v3.webhooks import (
     MessageEvent,
-    TextMessageContent,
-    UserSource
+    TextMessageContent
 )
 from linebot.models import (
     TextSendMessage
 )
+
+
+# line_idに紐づくデータを取得
+def get_data(line_id: str):
+    with LineCommunicationStateCrud() as line_communication_state_crud:
+        data = line_communication_state_crud.get(line_id)
+    return data.data
+
+
+# 処理状況
+def get_status(line_id: str):
+    # lineのidから処理状況のEnumの値を返して欲しい
+    with LineCommunicationStateCrud() as line_communication_state_crud:
+        registration_status = line_communication_state_crud.get(line_id)
+
+    return registration_status.state
+
+
+# line_idに紐づくデータを登録、ステータスの更新
+def set_data(line_id: str, state: str, data: dict):
+    with LineCommunicationStateCrud() as line_communication_state_crud:
+        line_communication_state_crud.set(
+            line_id,
+            LINECommunicationStateSchema(state=state, data=data)
+        )
+
 
 # define router
 router = APIRouter(
@@ -48,6 +76,24 @@ line_bot_api = AsyncMessagingApi(async_api_client)
 parser = WebhookParser(channel_secret)
 
 
+# "10101" 上1桁：1-ユーザの登録,2-定期メンタリング 上23桁：処理順 上45桁：同処理で分岐する場合1ずつ加算する
+class STATUS(Enum):
+    START = 10201  # ユーザの登録処理開始
+    GROUP_JOIN_SUCCESS = 10501  # グループへの参加が完了した状態
+    NAME_SUCCESS = 10601  # 氏名の登録が完了した状態
+    CHARACTER_CHOICE_SUCCESS = 10701  # キャラクター選択が完了した状態
+    GOAL_SUCCESS = 10801  # 目標の設定が完了した状態
+    INTERVAL_SUCCESS = 10901  # インターバルの設定が成功した時
+    INTERVAL_FAIL = 10902  # インターバルの設定が失敗した時
+    TARGET_SUCCESS = 11001  # 短期目標の設定が完了した時
+
+    # 現状使っていないもの
+    NO_PROCESS = 10101  # 登録処理が走っていない状態
+    ID_SUCCESS = 10301  # ユーザIDの登録がされた状態
+    GROUP_SEARCH_SUCCESS = 10401  # 正しいグループIDが入力された時
+    GROUP_SEARCH_FAIL = 10402  # 誤ったグループIDが入力された時
+
+
 @router.post("/callback")
 async def handle_callback(request: Request):
     signature = request.headers['X-Line-Signature']
@@ -66,29 +112,64 @@ async def handle_callback(request: Request):
             continue
         if not isinstance(ev.message, TextMessageContent):
             continue
+        line_id = ev.source.user_id
 
-        # 友達追加されたとき
-        # これはGUIで設定する
-        # if ev.type == "follow":
-        #     await line_bot_api.reply_message(
-        #         ReplyMessageRequest(
-        #             reply_token=ev.reply_token,
-        #             messages=[
-        #                 TextMessage(text="はじめると送ってください(follow)"),
-        #             ]
-        #         )
-        #     )
+        # 何もしていないとき
+        if get_status(line_id) == None:
+            if ev.message.text == "はじめる":
+                set_data(line_id, "START", {"line_id": line_id})
+                return "GroupIDを聞く"
+            else:
+                return "回答, はじめると送ってね"
+        
+        # GroupIDを聞く
+        elif get_status(line_id) == "START":
+            # 正しいグループIDかどうか
+            if ev.message.text == True:
+                return "GroupNameに参加しますか？" # カルーセルで
+            # グループに参加
+            elif ev.message.text == "参加する":
+                set_data(line_id, "GROUP_JOIN_SUCCESS", {"group_id": group_id})
+                return "GroupNameに参加しました!, 次にあなたの名前を教えてください"
+            # 誤ったグループIDの場合
+            else:
+                return "回答, グループIDが間違っているようです。もう一度正しいものを送ってください。"
+        # 氏名を聞く
+        elif get_status(line_id) == "GROUP_JOIN_SUCCESS":
+            if ev.message.text == 
+            
 
-        if ev.message.text == "はじめる":
-            await line_bot_api.reply_message(
-                ReplyMessageRequest(
-                    reply_token=ev.reply_token,
-                    messages=[
-                        TextMessage(text="success(はじめる)"),
-                    ]
-                )
-            )
-        else:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        reply_message_list = []
+
+        if get_status(line_id):
+            ev.message.text == "はじめる":
+            
+            reply_message_list.append(TextMessage(
+                text="GroupIDがあれば入力してください。\nなければなしと送信してください。"))
+
+        # ユーザが未登録andはじめると送られていない
+        if True:
             await line_bot_api.reply_message(
                 ReplyMessageRequest(
                     reply_token=ev.reply_token,
@@ -97,5 +178,13 @@ async def handle_callback(request: Request):
                     ]
                 )
             )
+
+        await line_bot_api.reply_message(
+            ReplyMessageRequest(
+                reply_token=ev.reply_token,
+                messages=reply_message_list
+            )
+        )
+        return 'OK'
 
     return 'OK'

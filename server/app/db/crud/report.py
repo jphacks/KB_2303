f42b61989d61b59597f4613ade946ec9d72d8dd3
Yuki Watanabe <ukwhatn@gmail.com>
2023-10-29 07:44:29 +0900

    [wip]

--- server/app/db/crud/report.py
+++ server/app/db/crud/report.py
@@ -7,7 +7,7 @@ from .. import models
 
 def get_need_to_process_scheduled_reports(db: Session) -> list[models.Report]:
     # scheduled_hearing_dateを過ぎているが、まだhearing_dateが設定されていないものを取得
-    res = (db.query(models.Report).join(models.User)
+    res = (db.query(models.Report).join(models.User).join(models.UserConfig)
            .filter(models.Report.scheduled_hearing_date <= datetime.now())
            .filter(models.Report.hearing_date == None)
            .all())

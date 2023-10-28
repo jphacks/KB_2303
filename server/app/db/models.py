10fbc1bf5ae4f5739a1e692698475f8ac240e40c
Yuki Watanabe <ukwhatn@gmail.com>
2023-10-29 06:56:18 +0900

    [add] 定期送信

--- server/app/db/models.py
+++ server/app/db/models.py
@@ -63,6 +63,7 @@ class User(Base):
 
     group_users: Mapped["GroupUser"] = relationship(back_populates="user")
     reports: Mapped[list["Report"]] = relationship(back_populates="user")
+    config: Mapped["UserConfig"] = relationship(back_populates="user")
 
     created_at = Column(DateTime, default=datetime.now(), nullable=False)
     updated_at = Column(DateTime, default=datetime.now(), onupdate=datetime.now(), nullable=False)
@@ -73,6 +74,7 @@ class UserConfig(Base):
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     user_id = Column(Integer, ForeignKey("user.id"), index=True, unique=True)
+    user = relationship("User", back_populates="config")
 
     interval_days = Column(Integer, nullable=False)
 

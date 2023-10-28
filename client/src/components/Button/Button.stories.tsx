69a6b9f8b633a04525e598e7dcb528c6d8a73eab
inatatsu <tatsuhiro.9699@gmail.com>
2023-10-28 17:21:23 +0900

    [add] Button component

--- client/src/components/Button/Button.stories.tsx
+++ client/src/components/Button/Button.stories.tsx
@@ -14,9 +14,63 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const Large: Story = {
+const mockFn = () => console.log('clicked')
+
+export const FillGreen: Story = {
   args: {
     color: 'green',
     type: 'Fill',
+    label: 'Submit',
+    onClick: mockFn,
+  },
+}
+export const OutlineGreen: Story = {
+  args: {
+    color: 'green',
+    type: 'Outline',
+    label: 'Submit',
+    onClick: mockFn,
+  },
+}
+export const FillRed: Story = {
+  args: {
+    color: 'red',
+    type: 'Fill',
+    label: 'Submit',
+    onClick: mockFn,
+  },
+}
+export const OutlineRed: Story = {
+  args: {
+    color: 'red',
+    type: 'Outline',
+    label: 'Submit',
+    onClick: mockFn,
+  },
+}
+export const Gray: Story = {
+  args: {
+    color: 'gray',
+    type: 'Fill',
+    label: 'Submit',
+    onClick: mockFn,
+  },
+}
+export const DisabledFill: Story = {
+  args: {
+    color: 'gray',
+    type: 'Fill',
+    label: 'Submit',
+    disabled: true,
+    onClick: mockFn,
+  },
+}
+export const DisabledOutline: Story = {
+  args: {
+    color: 'gray',
+    type: 'Outline',
+    label: 'Submit',
+    disabled: true,
+    onClick: mockFn,
   },
 }
--- client/src/components/Button/Styles.tsx
+++ client/src/components/Button/Styles.tsx
@@ -1,13 +1,60 @@
 import styled from '@emotion/styled'
 import { Color } from '../../utils/Color'
 
-export const Fill = styled('div')`
-  width: 100px;
-  height: 32px;
-  border-radius: 8px;
+export const Fill = styled('button')<{
+  backgroundColor: string
+  activeColor: string
+}>`
+  padding: 14px 24px;
+  border-radius: 5px;
   color: ${Color.white};
-  background: ${({ color }) => color};
+  background: ${({ backgroundColor }) => backgroundColor};
   display: flex;
   justify-content: center;
   align-items: center;
+  cursor: pointer;
+  border: none;
+  &:active {
+    background: ${({ activeColor }) => activeColor};
+  }
+`
+export const Outline = styled('button')<{
+  textColor: string
+  activeColor: string
+}>`
+  padding: 14px 24px;
+  border-radius: 5px;
+  border: 1px solid ${({ textColor }) => textColor};
+  color: ${({ textColor }) => textColor};
+  background: ${Color.white};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  &:active {
+    background: ${Color.gray[150]};
+  }
+`
+export const DisabledFill = styled('button')`
+  padding: 14px 24px;
+  border-radius: 5px;
+  color: ${Color.white};
+  background: ${Color.gray[250]};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: not-allowed;
+  border: none;
+`
+export const DisabledOutline = styled('button')`
+  padding: 14px 24px;
+  border-radius: 5px;
+  border: 1px solid ${Color.gray[250]};
+  border-radius: 5px;
+  background: ${Color.white};
+  color: ${Color.gray[250]};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: not-allowed;
 `

a8f17a7363993fb492c131d99c39ff70eac77431
iyom2020 <sosk020321@gmail.com>
2023-10-28 23:58:45 +0900

    [add] 目標達成率のパラメータコンポーネントを作成

---
+++ client/src/components/AchievementMeter/Styles.tsx
@@ -0,0 +1,64 @@
+import styled from '@emotion/styled'
+import { Color } from '../../utils/Color'
+
+export const Root = styled('div')`
+  width: 100px;
+  height: 100px;
+  position: relative;
+  display: flex;
+  justify-content: center;
+`
+
+export const RootText = styled('div')`
+  text-align: center;
+  font-size: 12px;
+  color: ${Color.gray[850]};
+  position: absolute;
+  transform: translate(0, -50%);
+  z-index: 4;
+`
+
+export const RootTextBack = styled('div')`
+  text-align: center;
+  font-size: 12px;
+  font-color: ${Color.white};
+  background-color: ${Color.white};
+  position: absolute;
+  transform: translate(0, -50%);
+  z-index: 2;
+`
+
+export const Flame = styled('div')`
+  width: 98px;
+  height: 98px;
+  position: absolute;
+  border: 1px solid ${Color.gray[850]};
+  border-radius: 4px;
+  z-index: 1;
+`
+
+export const Meter = styled('div')<{
+  par: number
+}>`
+  width: 98px;
+  max-height: 98px;
+  height: ${({par}) => par - 2}px;
+  position: absolute;
+  background: ${({par}) => par < 40 ? Color.red.main: Color.green.main};
+  border-radius: 3px;
+  top: 100%;
+  transform: translate(0, calc(-100% - 1px));
+  z-index: 3;
+`
+
+export const MeterText = styled('div')`
+  font-size: 32px;
+  text-align: center;
+  color: ${Color.gray[900]};
+  width: 100px;
+  position: absolute;
+  border-radius: 4px;
+  top: 50%;
+  transform: translate(0, -50%);
+  z-index: 4;
+`

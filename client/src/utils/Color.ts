71eacbeeee17695ec63aa03c41066ca6d298ebc9
inatatsu <tatsuhiro.9699@gmail.com>
2023-10-28 16:56:02 +0900

    [add] カラーパレットの編集

--- client/src/utils/Color.ts
+++ client/src/utils/Color.ts
@@ -15,6 +15,23 @@ export const Color = {
   },
   white: '#ffffff',
   black: '#000000',
-  green: '06C755',
-  red: '#FF334B',
+  green: {
+    main: '#06C755',
+    active: '#1E6E1E',
+  },
+  red: {
+    main: '#FF334B',
+    active: '#C9162B',
+  },
+}
+
+export const hex2rgb = (hex: string) => {
+  return hex2rgba(hex, 1)
+}
+
+export const hex2rgba = (hex: string, alpha: number) => {
+  const arr = [hex.slice(1, 3), hex.slice(3, 5), hex.slice(5, 7)].map((str) =>
+    parseInt(str, 16)
+  )
+  return `rgb(${arr[0]} ${arr[1]} ${arr[2]} / ${alpha})`
 }

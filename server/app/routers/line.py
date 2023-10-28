40ec871382fd807c91f8fd7e3f0cf5a7efbba168
krn-7717 <104199555+krn-7717@users.noreply.github.com>
2023-10-29 01:16:56 +0900

    [add]登録処理

--- server/app/routers/line.py
+++ server/app/routers/line.py
@@ -79,12 +79,13 @@ class STATUS(Enum):
     # 1xxxx: ユーザの登録
     INPUT_GROUP_ID = 10101  # グループIDを聞く
     CONFIRM_GROUP_JOIN = 10102  # グループに参加するか確認
-    INPUT_NAME = 10201  # 氏名を聞く
-    INPUT_GOAL = 10301  # 目標を聞く
-    INPUT_INTERVAL = 10401  # メンタリングの頻度を聞く
-    INPUT_TARGET = 10501  # 短期目標を聞く
-    CONFIRM_REGISTRATION = 10601  # 登録内容を確認
-    CONFIRM_RETURN_TO_INPUT_NAME = 10602  # 氏名の入力に戻るか確認
+    SERECT_CHARACTER = 10201
+    INPUT_NAME = 10301  # 氏名を聞く
+    INPUT_GOAL = 10401  # 目標を聞く
+    INPUT_INTERVAL = 10501  # メンタリングの頻度を聞く
+    INPUT_TARGET = 10601  # 短期目標を聞く
+    CONFIRM_REGISTRATION = 10701  # 登録内容を確認
+    CONFIRM_RETURN_TO_INPUT_NAME = 10702  # 氏名の入力に戻るか確認
 
 
 @router.post("/callback")
@@ -131,7 +132,8 @@ async def handle_callback(
             )
 
         # stateを確認する
-        saved_data: LINECommunicationStateSchema | None = get_saved_data(line_id)
+        saved_data: LINECommunicationStateSchema | None = get_saved_data(
+            line_id)
 
         # ----end of debug-----
 
@@ -198,10 +200,10 @@ async def handle_callback(
                 if input_text == "はい":
                     # 氏名を聞く
                     reply_message_list.append(TextMessage(
-                        text="氏名を入力してください")
+                        text="キャラクターを選択してください")
                     )
                     # 状態を更新
-                    saved_data.state = STATUS.INPUT_NAME.name
+                    saved_data.state = STATUS.SERECT_CHARACTER.name
                     set_saved_data(line_id, saved_data)
 
                 # 参加しない場合
@@ -220,6 +222,203 @@ async def handle_callback(
                     # グループに参加するか確認する
                     reply_message_list.append(TextMessage(
                         text="「はい」か「いいえ」でお答えください。")
+                    )
+            # キャラクターを選択していた場合
+            elif saved_status == STATUS.SERECT_CHARACTER:
+                if input_text == "":
+                    reply_message_list.append(TextMessage(
+                        text="キャラクターを選択してください"
+                    ))
+                else:
+                    # 氏名を聞く
+                    reply_message_list.append(TextMessage(
+                        text="氏名を入力してください"
+                    ))
+                    # 状態を更新
+                    saved_data.state = STATUS.INPUT_NAME.name
+                    saved_data.data["character_id"] = input_text
+                    set_saved_data(line_id, saved_data)
+
+            # 氏名を聞いていた場合
+            elif saved_status == STATUS.INPUT_NAME:
+                if input_text == "":
+                    reply_message_list.append(TextMessage(
+                        text="氏名を入力してください")
+                    )
+                else:
+                    # 目標を聞く
+                    reply_message_list.append(TextMessage(
+                        text="目標を入力してください")
+                    )
+                    # 状態を更新
+                    saved_data.state = STATUS.INPUT_GOAL.name
+                    saved_data.data["name"] = input_text
+                    set_saved_data(line_id, saved_data)
+
+            # 目標を聞いていた場合
+            elif saved_status == STATUS.INPUT_GOAL:
+                if input_text == "":
+                    reply_message_list.append(TextMessage(
+                        text="目標を入力してください")
+                    )
+                else:
+                    # メンタリングの頻度を聞く
+                    reply_message_list.append(TemplateMessage(
+                        alt_text=f"メンタリングの頻度を選択してください",
+                        template=ButtonsTemplate(
+                            text=f"メンタリングの頻度を選択してください",
+                            actions=[
+                                MessageAction(
+                                    label="毎日",
+                                    text="1"
+                                ),
+                                MessageAction(
+                                    label="3日に1回",
+                                    text="3"
+                                ),
+                                MessageAction(
+                                    label="週に1回",
+                                    text="7"
+                                )
+                            ]
+                        )
+                    ))
+                    # 状態を更新
+                    saved_data.state = STATUS.INPUT_INTERVAL.name
+                    saved_data.data["goal"] = input_text
+                    set_saved_data(line_id, saved_data)
+
+            # メンタリングの頻度を聞いていた場合
+            elif saved_status == STATUS.INPUT_INTERVAL:
+                if input_text not in ["1", "3", "7"]:
+                    reply_message_list.append(TextMessage(
+                        text="上のボタンからメンタリングの頻度を選択してください")
+                    )
+                else:
+                    # 短期目標を聞く
+                    reply_message_list.append(TextMessage(
+                        text=f"{input_text}日後までの短期目標を入力してください")
+                    )
+                    # 状態を更新
+                    saved_data.state = STATUS.INPUT_TARGET.name
+                    saved_data.data["interval_days"] = int(input_text)
+                    set_saved_data(line_id, saved_data)
+
+            # 短期目標を聞いていた場合
+            elif saved_status == STATUS.INPUT_TARGET:
+                if input_text == "":
+                    reply_message_list.append(TextMessage(
+                        text=f'{saved_data.data["interval_days"]}日後までの短期目標を入力してください')
+                    )
+                else:
+                    # 状態を代入
+                    saved_data.state = STATUS.CONFIRM_REGISTRATION.name
+                    saved_data.data["target"] = input_text
+
+                    # 登録内容を確認
+                    reply_text = "\n".join([
+                        f'氏名: {saved_data.data["name"]}',
+                        f'目標: {saved_data.data["goal"]}',
+                        f'メンタリングの頻度: {saved_data.data["interval_days"]}',
+                        f'{saved_data.data["interval_days"]}日後までの目標: {saved_data.data["target"]}'
+                    ])
+                    reply_message_list.append(TemplateMessage(
+                        alt_text=f"下記の内容でよろしいですか？\n{reply_text}",
+                        template=ButtonsTemplate(
+                            text=f"下記の内容でよろしいですか？\n{reply_text}",
+                            actions=[
+                                MessageAction(
+                                    label="確定する",
+                                    text="確定する"
+                                ),
+                                MessageAction(
+                                    label="修正する",
+                                    text="修正する"
+                                )
+                            ]
+                        )
+                    ))
+                    # 状態を更新
+                    set_saved_data(line_id, saved_data)
+
+            # 登録内容の確認をしていた場合
+            elif saved_status == STATUS.CONFIRM_REGISTRATION:
+                if input_text == "確定する":
+                    # TODO: DBに登録して!!!!
+
+                    # cacheを削除
+                    delete_saved_data(line_id)
+                    reply_message_list.append(TextMessage(
+                        text="登録が完了しました")
+                    )
+                elif input_text == "修正する":
+                    # 氏名の入力に戻るか確認
+                    reply_message_list.append(TemplateMessage(
+                        alt_text=f"氏名の入力にもどりますが、本当によろしいですか？",
+                        template=ButtonsTemplate(
+                            text=f"氏名の入力にもどりますが、本当によろしいですか？",
+                            actions=[
+                                MessageAction(
+                                    label="やり直す",
+                                    text="やり直す"
+                                ),
+                                MessageAction(
+                                    label="確認にもどる",
+                                    text="確認にもどる"
+                                )
+                            ]
+                        )
+                    ))
+                    # 状態を更新
+                    saved_data.state = STATUS.CONFIRM_RETURN_TO_INPUT_NAME.name
+                    set_saved_data(line_id, saved_data)
+                else:
+                    reply_message_list.append(TextMessage(
+                        text="上のボタンから選択してください")
+                    )
+
+            # 氏名の入力に戻るか確認をしていた場合
+            elif set_saved_data == STATUS.CONFIRM_RETURN_TO_INPUT_NAME:
+                if input_text == "やり直す":
+                    # 氏名を聞く
+                    reply_message_list.append(TextMessage(
+                        text="氏名を入力してください"
+                    ))
+                    # 状態を更新
+                    saved_data.state = STATUS.INPUT_NAME.name
+                    set_saved_data(line_id, saved_data)
+                elif input_text == "確認にもどる":
+                    # 状態を代入
+                    saved_data.state = STATUS.CONFIRM_REGISTRATION.name
+
+                    # 登録内容を確認
+                    reply_text = "\n".join([
+                        f'氏名: {saved_data.data["name"]}',
+                        f'目標: {saved_data.data["goal"]}',
+                        f'メンタリングの頻度: {saved_data.data["interval_days"]}',
+                        f'{saved_data.data["interval_days"]}日後までの目標: {saved_data.data["target"]}'
+                    ])
+                    reply_message_list.append(TemplateMessage(
+                        alt_text=f"下記の内容でよろしいですか？\n{reply_text}",
+                        template=ButtonsTemplate(
+                            text=f"下記の内容でよろしいですか？\n{reply_text}",
+                            actions=[
+                                MessageAction(
+                                    label="確定する",
+                                    text="確定する"
+                                ),
+                                MessageAction(
+                                    label="修正する",
+                                    text="修正する"
+                                )
+                            ]
+                        )
+                    ))
+                    # 状態を更新
+                    set_saved_data(line_id, saved_data)
+                else:
+                    reply_message_list.append(TextMessage(
+                        text="上のボタンから選択してください")
                     )
 
         # テスト

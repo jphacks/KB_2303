7bc664ad577a4f69f751a7a7bc146bd152d25390
iyom2020 <sosk020321@gmail.com>
2023-10-29 14:05:59 +0900

    [add] グループ情報パネルを作成

--- client/src/components/SettingPanel/Styles.tsx
+++ client/src/components/SettingPanel/Styles.tsx
@@ -1,7 +1,30 @@
 import styled from '@emotion/styled'
+import { Color } from '../../utils/Color'
 
 export const Root = styled('div')`
   display: flex;
-  align-items: center;
+  flex-direction: column;
+  align-items: start;
   padding: 24px;
+`
+
+export const PanelTitle = styled('div')`
+  font-size: 28px;
+  font-weight: boald;
+  margin-bottom: 20px;
+`
+
+export const SettingTitle = styled('div')`
+  font-size: 12px;
+`
+
+export const IDView = styled('div')`
+  font-size: 28px;
+  border: 1px solid ${Color.gray[850]};
+  border-radius: 4px;
+  padding: 4px 10px;
+`
+
+export const HeightMargin = styled('div')`
+  height: 15px
 `

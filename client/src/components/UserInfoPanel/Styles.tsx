bc28cc4f6eecfc14febaa898a6197172df3be5ca
inatatsu <tatsuhiro.9699@gmail.com>
2023-10-29 13:06:39 +0900

    [add] avater card

--- client/src/components/UserInfoPanel/Styles.tsx
+++ client/src/components/UserInfoPanel/Styles.tsx
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled'
+import { Color } from '../../utils/Color'
 
 export const Root = styled('div')`
   display: flex;
@@ -10,6 +11,12 @@ export const Center = styled('div')`
   flex-grow: 1;
   height: 100vh;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
+`
+
+export const Text = styled('div')`
+  font-size: 16px;
+  color: ${Color.gray[900]};
 `
